ca3912f109b43b9a4736c3e8ed6867a63547d7d0
Aruba <aruba0397@gmail.com>
2025-08-19 15:24:26 +0500

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -18,3 +19,22 @@ def save_object(file_path, obj):
     
     except Exception as e:
         raise CustomeException(e,sys)
+    
+
+def evalute_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report ={}
+        
+        for name, model in models.items():  
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            score = r2_score(y_test, y_pred)
+            report[name] = score
+        
+            
+        return report
+    
+
+    except Exception as e:
+        raise CustomException(e,sys)
+        
